00746eff6cbb01037e9875c9912799bb29a0ede2
Eric Mertens <emertens@gmail.com>
2015-03-26 00:25:24 -0700

    possibly simpler layout pass

--- src/ConfigFile/Tokens.hs
+++ src/ConfigFile/Tokens.hs
@@ -38,44 +38,30 @@ data Token
 layoutPass ::
   [PosToken] {- ^ tokens without layout markers -} ->
   [PosToken] {- ^ tokens with    layout markers -}
-layoutPass = layoutPass' [1]
+layoutPass toks = foldr step (\_ _ -> []) toks False [1]
 
--- | Worker function for 'layoutPass'. This function keeps
--- a stack of the current layout scopes. New scopes are
--- opened for layout herald tokens and the scopes are closed
--- when a token occurs at or before the marked column.
-layoutPass' ::
-  [Int]      {- ^ layout column stack -} ->
-  [PosToken] {- ^ remaining tokens    -} ->
-  [PosToken]
+step :: PosToken -> (Bool -> [Int] -> [PosToken]) -> Bool -> [Int] -> [PosToken]
 
-layoutPass' _ []  = []
+-- start blocks must be indented
+-- tokens before the current layout end the current layout
+step t next startBlock (col:cols)
+  | posColumn t <= col && startBlock = [t {posToken = Error}]
+  | posColumn t < col = t {posToken = LayoutEnd} : step t next startBlock cols
 
--- Layout ends when a token occurs BEFORE the current layout scope.
-layoutPass' (col:cols) (t:ts)
-  | posColumn t < col
-  = t { posToken = LayoutEnd } : layoutPass' cols (t:ts)
+-- non-start layout needs to land on the current block
+step t _ False (col:_)
+  | usesLayout t && posColumn t /= col = [t {posToken = Error}]
 
--- Layout FAILS when a token occurs AFTER the current layout scope
--- This is quite restrictive but the language is simple enough
--- that it's OK.
-layoutPass' (col:_) (t:ts)
-  | col < posColumn t = t { posToken = Error } : t : ts
-
--- Layout extends out to the token after token.
-layoutPass' stack (t1:t2:ts)
-  | isLayoutHerald t1
-  = if posColumn t1 < posColumn t2
-     then t1 : layoutPass' (posColumn t2:stack) (t2:ts)
-     else t1 : t2 { posToken = Error } : t2 : ts
-
-layoutPass' stack (t:ts) = t : layoutPass' stack ts
-
+step t next startBlock cols =
+  t : next (usesLayout t) cols'
+  where
+  cols' | startBlock = posColumn t : cols
+        | otherwise  = cols
 
 
 -- | Return True when a token starts a layout scope.
-isLayoutHerald :: PosToken -> Bool
-isLayoutHerald t
+usesLayout :: PosToken -> Bool
+usesLayout t
   | Section{} <- posToken t = True
   | Bullet    <- posToken t = True
   | otherwise               = False
