8dddb1aab232f12fff6859e0242c8efb3253d65b
Iavor S. Diatchki <diatchki@galois.com>
2015-03-26 17:57:01 -0700

    Print numbers at the given base.

--- src/Config/Pretty.hs
+++ src/Config/Pretty.hs
@@ -1,9 +1,10 @@
 module Config.Pretty where
 
-import           Data.Char (isPrint, isDigit)
+import           Data.Char (isPrint, isDigit,intToDigit)
 import           Data.List (mapAccumL)
 import qualified Data.Text as Text
 import           Text.PrettyPrint
+import           Numeric(showIntAtBase)
 
 import Config.Value
 
@@ -12,11 +13,24 @@ pretty value =
   case value of
     Sections [] -> text "{}"
     Sections xs -> vcat (map prettySection xs)
-    Number n    -> integer n
+    Number b n  -> prettyNum b n
     Text t      -> prettyText (Text.unpack t)
     Bool b      -> if b then text "yes" else text "no"
     List []     -> text "[]"
     List xs     -> vcat [ char '*' <+> pretty x | x <- xs ]
+
+
+prettyNum :: Int -> Integer -> Doc
+prettyNum b n
+  | b == 16   = pref <> text "0x" <> num
+  | b ==  8   = pref <> text "0o" <> num
+  | b ==  2   = pref <> text "0b" <> num
+  | otherwise = integer n
+  where
+  pref = if n < 0 then char '-' else empty
+  num  = text (showIntAtBase (fromIntegral b) intToDigit (abs n) "")
+
+
 
 prettyText :: String -> Doc
 prettyText = doubleQuotes . cat . snd . mapAccumL ppChar True
